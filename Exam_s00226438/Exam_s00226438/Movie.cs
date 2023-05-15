e8eee73427ddd021673689d5d0389852a1d2d2d1
Jakub Lodzinski <98521175+S00226438@users.noreply.github.com>
2023-05-15 16:53:38 +0100

    save

---
+++ Exam_s00226438/Exam_s00226438/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_s00226438
+{
+    internal class Movie
+    {
+        int MovieID;
+        string Title;
+        string Imagename;
+        string Description;
+        string Cast;
+    }
+}
